20aa29bb7241762812df54f98732b53bd05c0db1
Theorhetically <56592882+Theorhetically@users.noreply.github.com>
2019-11-04 21:33:24 +0800

    Add files via upload

--- Waifu.h
+++ Waifu.h
@@ -1,15 +1,20 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include "WaifuType.h"
 #include <string>
+#include <iostream>
 
 using namespace sf;
+using namespace std;
 
 class Waifu {
 private:
-	int nAffection;
 	char sName[15];
 	Sprite sImage;
+	string MiscDialogue[3];
+	string HappiDialogue[3];
+	string AngeryDialogue[3];
 
 	//Set to sprite when the user chooses to change outfit of Waifu
 	Texture tDefault,
@@ -19,7 +24,10 @@ private:
 	Vector2f vPos;
 
 public:
-	Waifu(char Name[15], Vector2f Pos) {
+
+	int nAffection;
+
+	Waifu(char Name[15], Vector2f Pos, WaifuType WType) {
 		//If it hits 0, Waifu runs away
 		//If it hits 100, Waifu is here to stay
 		//Outfits are unlocked also based on Waifu Affection level
@@ -28,10 +36,115 @@ public:
 		strcpy(sName, Name);
 		vPos = Pos;
 
-		sImage.setTexture(tDefault);
+		//Sprite and dialogue loading depending on type of waifu chosen to be created
+		if (WType == WaifuType::Neko) {
+
+			//sprite loading
+			if (!tDefault.loadFromFile("Assets\\Chen_character\\Chen_1.png")) {
+				cout << "Failed to load Chen_1";
+			}
+
+			if (!tUnlock1.loadFromFile("Assets\\Chen_character\\Chen_3.png")) {
+				cout << "Failed to load Chen_3";
+			}
+
+			if (!tUnlock2.loadFromFile("Assets\\Chen_character\\Chen_4.png")) {
+				cout << "Failed to load Chen_4";
+			}
+
+			//dialogue loading
+			MiscDialogue[0] = "*sniffing*";
+			MiscDialogue[1] = "Konichiwassup~";
+			MiscDialogue[2] = "Everything will be daijoubu";
+
+			HappiDialogue[0] = "*happiness meow*";
+			HappiDialogue[1] = "*rubs against you*";
+			HappiDialogue[2] = "Arigathanks Gozaimasyou~";
+
+			AngeryDialogue[0] = "*hissss*";
+			AngeryDialogue[1] = "Master will get you for that!";
+			AngeryDialogue[2] = "NOT DAIJOUBU!";
+			
+		}
+		else if (WType == WaifuType::Dog) {
+			if (!tDefault.loadFromFile("Assets\\Kyouko_character\\Kyouko_1.png")) {
+				cout << "Failed to load doggo";
+			}
+
+			if (!tUnlock1.loadFromFile("Assets\\Kyouko_character\\Kyouko_1.png")) {
+				cout << "Failed to load doggo";
+			}
+
+			if (!tUnlock2.loadFromFile("Assets\\Kyouko_character\\Kyouko_1.png")) {
+				cout << "Failed to load doggo";
+			}
+
+			MiscDialogue[0] = "*sniffing*";
+			MiscDialogue[1] = "*sweeping outside*";
+			MiscDialogue[2] = "Master Byakuren would like this place";
+
+			HappiDialogue[0] = "*licking you*";
+			HappiDialogue[1] = "*happiness woof*";
+			HappiDialogue[2] = "*happi smile*";
+
+			AngeryDialogue[0] = "*growl*";
+			AngeryDialogue[1] = "*lying down*";
+			AngeryDialogue[2] = "*whimpering*";
+		}
+		else if (WType == WaifuType::ShrineMaiden) {
+			if (!tDefault.loadFromFile("Assets\Reimu_character\\Reimu_1.png")) {
+				cout << "Failed to load Reimu_1";
+			}
+
+			if (!tUnlock1.loadFromFile("Assets\Reimu_character\\Reimu_3.png")) {
+				cout << "Failed to load Reimu_3";
+			}
+
+			if (!tUnlock2.loadFromFile("Assets\Reimu_character\\Reimu_4.png")) {
+				cout << "Failed to load Reimu_4";
+			}
+
+			MiscDialogue[0] = "*sipping tea*";
+			MiscDialogue[1] = "*sleeping*";
+			MiscDialogue[2] = "I wonder how Marisa's doing...";
+
+			HappiDialogue[0] = "Thank you very much~";
+			HappiDialogue[1] = "Maybe I won't need donations anymore~";
+			HappiDialogue[2] = "*happiness chuckle*";
+
+			AngeryDialogue[0] = "Seriously?";
+			AngeryDialogue[1] = "Heh, fairies can hit harder";
+			AngeryDialogue[2] = "If I hit back, You'd be on the other side of the world";
+		}
+		else if (WType == WaifuType::Yama) {
+			if (!tDefault.loadFromFile("Assets\Eiki_character\\Eiki_1.png")) {
+				cout << "Failed to load Eiki_1";
+			}
+
+			if (!tUnlock1.loadFromFile("Assets\Eiki_character\\Eiki_3.png")) {
+				cout << "Failed to load Eiki_3";
+			}
+
+			if (!tUnlock2.loadFromFile("Assets\Eiki_character\\Eiki_4.png")) {
+				cout << "Failed to load Eiki_4";
+			}
+
+			MiscDialogue[0] = "*sitting silently*";
+			MiscDialogue[1] = "Are you atoning for your sins?";
+			MiscDialogue[2] = "*sips tea*";
+
+			HappiDialogue[0] = "Th-this won't excempt you from judgement...";
+			HappiDialogue[1] = "I may be the Yama, but...";
+			HappiDialogue[2] = "I guess this is okay every once and a while...";
+
+			AngeryDialogue[0] = "How dare you?!";
+			AngeryDialogue[1] = "You sins continue to increase...";
+			AngeryDialogue[2] = "Judgement will be swift for you...";
+		}
 	}
 
-	~Waifu() {};
+	~Waifu() {
+	};
 
 	//Getters
 	Vector2f getPos() {
